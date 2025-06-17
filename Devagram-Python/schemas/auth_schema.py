8395ec6831a14d2dac7d13d6ccecf6e6f2275c0b
unknown <bahamuth001@gmail.com>
2025-06-17 19:01:10 -0300

    Update project

--- Devagram-Python/schemas/auth_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr, Field
-
-class LoginSchema(BaseModel):
-    email: EmailStr
-    senha: str = Field(..., min_length=6)
-
-class TokenSchema(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
